841f0cee6b3c3b6d888849fa4da949bae4aec81a
Gabriel Macedo <Gabrielmacedo097430@gmail.com>
2026-04-08 09:43:55 -0300

    Update Identificação da Ocorrência.c

--- Identificação da Ocorrência.c
+++ Identificação da Ocorrência.c
@@ -1,71 +1,81 @@
 #include <stdio.h>
 #include <locale.h>
 #include <string.h>
-int main(){
-setlocale(LC_ALL,"PORTUGUESE");
 
-	char Tipo[50];
-	char Data[20];
-	char Hora[20];
-	char Local[50];
-	int opcao;
+typedef struct{
+char Tipo[50];
+char Data[20];
+char Hora[20];
+char Local[50];
+}Ocorrencia;
+
+void cadastrarOcorrencia(Ocorrencia *O){
+int opcao;
+printf("Digite o Numero da Ocorrencia:\n");
+printf("1-Furto\n2-Roubo\n3-Homicidio\n4-Feminicidio\n5-Acidente\n6-Desaparecimento\n7-Nao criminal\n");
+scanf("%d", &opcao);
+getchar();
+
+switch(opcao) {
+
+	case 1:
+		strcpy(O->Tipo,"Furto\n");
+	break;
 	
-	printf("Digite o Numero da Ocorrencia:\n");
-		printf("1-Furto\n2-Roubo\n3-Homicidio\n4-Feminicidio\n5-Acidente\n6-Desaparecimento\n7-Nao criminal\n");
-			scanf("%d", &opcao);
+	case 2:
+		strcpy(O->Tipo,"Roubo\n");
+	break;
 		
-	switch(opcao) {
-		
-		case 1:
-			strcpy(Tipo,"Furto\n");
-		break;
-		
-		case 2:
-			strcpy(Tipo,"Roubo\n");
-		break;
-			
-		case 3:
-			strcpy(Tipo,"Homicidio\n");
-		break;
-		
-		case 4:
-			strcpy(Tipo,"Feminicidio\n");
-		break;
-			
-		case 5:
-			strcpy(Tipo,"Acidente\n");
-		break;
-		
-		case 6:
-			strcpy(Tipo,"Desaparecimento\n");
-		break;
-			
-		case 7:
-			strcpy(Tipo,"Não criminal\n");
-		break;
-		
-		default:
-			printf("Invalido\n");
-		
-	}
-	getchar();
+	case 3:
+		strcpy(O->Tipo,"Homicidio\n");
+	break;
 	
-	printf("Digite a Data do Ocorrido:\n");
-		fgets(Data, 30, stdin);
+	case 4:
+		strcpy(O->Tipo,"Feminicidio\n");
+	break;
 		
+	case 5:
+		strcpy(O->Tipo,"Acidente\n");
+	break;
+	
+	case 6:
+		strcpy(O->Tipo,"Desaparecimento\n");
+	break;
 		
-	printf("Digite a Hora do Ocorrido:\n");
-		fgets(Hora, 30, stdin);
+	case 7:
+		strcpy(O->Tipo,"Não criminal\n");
+	break;
+	
+	default:
+		printf(O->Tipo,"Invalido\n");
+}
+
+printf("Digite a Data do Ocorrido:\n");
+	fgets(O->Data, 30, stdin);
 	
 	
-	printf("Digite o Local do Ocorrido:\n");
-		fgets(Local, 100, stdin);
-		
-	printf("\n---dados adicionados até agora---\n");
-		printf("Tipo: %s\n", Tipo);
-		printf("Data: %s\n", Data);
-		printf("Hora: %s\n", Hora);
-		printf("Local: %s\n", Local);
-	
- return 0;
+printf("Digite a Hora do Ocorrido:\n");
+	fgets(O->Hora, 30, stdin);
+
+
+printf("Digite o Local do Ocorrido:\n");
+	fgets(O->Local, 100, stdin);
+}
+
+void mostrarOcorrencia(Ocorrencia O){
+printf("\n---dados adicionados até agora---\n");
+printf("Tipo: %s\n", O.Tipo);
+printf("Data: %s\n", O.Data);
+printf("Hora: %s\n", O.Hora);
+printf("Local: %s\n",O.Local);
+}
+int main(){
+setlocale(LC_ALL,"portuguese");
+
+Ocorrencia ocorrencia;
+
+cadastrarOcorrencia(&ocorrencia);
+mostrarOcorrencia(ocorrencia);
+
+return 0;
 }
